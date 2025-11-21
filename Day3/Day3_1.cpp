cc577917f287c6e500f1f3568dc42b641ca8d3cf
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-11-21 16:25:26 +0000

    update

---
+++ Day3/Day3_1.cpp
@@ -0,0 +1,15 @@
+//using typedef keyword
+#include <iostream>
+typedef unsigned short int Short;
+
+int main(){
+    using std:: cout;
+    using std:: endl;
+    Short Width = 5;
+    cout<<Width<<endl;
+
+
+
+
+    return 0 ;
+}
