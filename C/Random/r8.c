113985877377c7734298c0357047a43e34c5440a
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-01-08 08:41:16 +0000

    update

--- C/Random/r8.c
+++ C/Random/r8.c
@@ -3,11 +3,11 @@
 // simple program representing arrays
 
 int main(){
-    int num[5] = {20,10,12,22,20};
-    int length = sizeof(num)/sizeof(num[0]);
+    int age[5] = {20,10,12,22,20};
+    int length = sizeof(age)/sizeof(age[0]);
 
     for(int i =0; i<length;i++){
-        printf("The person %d is %d old ", i+1, num[i]);
+        printf("The person %d is %d old ", i+1, age[i]);
         printf("\n");
     }
 
