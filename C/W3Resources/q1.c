400b8b0454cec3f5782987d7e004963a7dcb4fd4
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-01-26 16:12:13 +0000

    for practice update

---
+++ C/W3Resources/q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+// 1. Print name, DOB, and mobile number
+
+
+    printf("%s,%s,%ld", " Name: Name","\n DOB: M D Y \n Mobile: ", 000000000 );
+
+
+
+    return 0;
+}
