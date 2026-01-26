a64b88f57a845635d1092c0a10cc2f156ffd791d
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-01-26 16:22:17 +0000

    for practice update

---
+++ C/W3Resources/q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // // q3 Print large 'C' 
+
+    int i = 0;
+    int j = 0;
+
+    for(;i<10;i++){
+        printf("#");
+        for(;j<10;j++){
+            printf("#");
+        }
+        printf("#");
+        printf("\n");
+    }
+    for(i=0;i<10;i++){
+            printf("#");
+
+        }
+
+}
