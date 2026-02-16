ef5f9e5bbaa060b8f014b7a8419f23035acded54
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-02-16 22:25:54 +0000

    update

--- Projects/ci1.c
+++ Projects/ci1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, const char *argv[]) {
-    // 1. Check if the user provided enough arguments
+    // Check if the user provided enough arguments
     if (argc < 3) {
         printf("Usage: %s <cleartextfile> <secrettextfile>\n", argv[0]);
         return 1;
@@ -11,11 +11,9 @@ int main(int argc, const char *argv[]) {
     const char *filename1 = argv[1];
     const char *filename2 = argv[2];
 
-    // 2. Open files
     FILE *file1 = fopen(filename1, "r");
     FILE *file2 = fopen(filename2, "r");
 
-    // 3. CRITICAL: Check if files actually opened
     // This prevents the Segmentation Fault if a file is missing
     if (file1 == NULL || file2 == NULL) {
         fprintf(stderr, "Error: Could not open one or both files.\n");
@@ -26,16 +24,12 @@ int main(int argc, const char *argv[]) {
 
     int c1, c2, cx;
 
-    /* 4. FIX PARENTHESES: 
-       Original: (c1 = fgetc(file1) != EOF) -> sets c1 to 0 or 1
-       Fixed:    ((c1 = fgetc(file1)) != EOF) -> sets c1 to the actual character
-    */
     while (((c1 = fgetc(file1)) != EOF) && ((c2 = fgetc(file2)) != EOF)) {
         cx = c2 ^ c1;
         putc(cx, stdout);
     }
 
-    // 5. Clean up
+    // Clean up
     fclose(file1);
     fclose(file2);
 
